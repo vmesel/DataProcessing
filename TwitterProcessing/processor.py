bc84b074089c68c8e640063b6ef67a6503ca46a7
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-04-14 18:52:06 -0300

    Parsing inteligente

--- TwitterProcessing/processor.py
+++ TwitterProcessing/processor.py
@@ -1,38 +1,52 @@
-# Processador de Linguagem Natural baseado em Tweets!
 """
     About twitterprocessor
-Version:    0.0.1 (April/2016)
+Description: Processador de Linguagem Natural baseado em Tweets!
+Version:    0.0.2 (April/2016)
+Last Mod:   14-04-16
 Author:     Vinicius Mesel (vmesel)
 Oficial:    Github.com/vmesel/DataProcessing/
 License:    MIT
 Support:    Python 3+
 """
+
 from twitter import *
 import settings
-import urllib.request
+import json
 
 t = Twitter(auth=OAuth(settings.token, settings.tokensecret, settings.consumerkey, settings.consumersecret))
 s = t.search.tweets(q="#guerracivil")
 
-#for tweet in s:
-#    print(tweet['text'])
 
-print(s[0])
-
-#with open("twitter-return.txt","a") as f:
-    #print(str(f.read()))
-#    data = f.read()
-
-#    print(json.load(data))
-    #p = f.read()
-    #print(p)
-    #json.load(p)
-    #file = f.read()
-    #j = json.load(file)
-
-#    f.close()
+statuses = s['statuses']
 
 
+for status in statuses:
+    if "RT" in status['text']:
+        status.clear()
+    else:
+        if "pt" in status['lang']:
+
+            print("########################################### Tweet ########################################### ")
+            print("Tweet ID: " + str(status['id']))
+            print("Hora: " + status['created_at'])
+            print(status['user']['screen_name'])
+            print(status['source'])
+            hashtags = []
+            for hashtag in status['entities']['hashtags']:
+                hashtags.append(hashtag['text'])
+            print(hashtags)
+            print(status['retweet_count'])
+            print(status['favorite_count'])
+            palavras = []
+
+
+            for palavra in status['text'].split(" "):
+                if "#" in palavra:
+                    pass
+                else:
+                    palavras.append(palavra)
+            print(palavras)
+        # Aqui que nós vamos implementar a parte de parseamento de palavras e definição de adjetivos
 
 
 # Este é um recurso útil para podermos postar toda vez que nosso crawler parsear algo de interessante
