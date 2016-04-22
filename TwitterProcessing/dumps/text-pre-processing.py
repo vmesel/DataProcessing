c253eb01a61f612e33270af4df7cee8532bf406e
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-04-22 12:03:31 -0300

    New Things
    We have the TXT files to analyse, new python processors and new project
    structure!

--- TwitterProcessing/dumps/text-pre-processing.py
+++ TwitterProcessing/dumps/text-pre-processing.py
@@ -4,22 +4,23 @@ import nltk
 
 regex = compile("\w+")
 
-arq = open("database2.txt")
+arq = open("dbs/database.txt")
 
 # ----------- OP1
-    artigos = "A O AS OS".split()
+artigos = "A O AS OS".split()
 
-    for x in arq:
-        x = regex.findall(x)
-        try:
-            if x[0] in artigos:
-                pessoa = x[1]
-                resto = x[2:]
-                print(x[0], pessoa, resto)
-        except:
-            pass
+for x in arq:
+    x = regex.findall(x)
+    try:
+        if x[0] in artigos:
+            pessoa = x[1]
+            resto = x[2:]
+            print(x[0], pessoa, resto,"\n")
+    except:
+        pass
 
 # ----------- OP2
+"""
 lista = []
 for x in arq:
     saida = regex.findall(x.lower())
@@ -27,8 +28,9 @@ for x in arq:
         lista.append(y)
 
 print(Counter(lista))
-
+"""
 # ----------- OP3
+"""
 texto = arq.read()
 
 a = nltk.wordpunct_tokenize(texto)
@@ -37,3 +39,4 @@ a = nltk.Text(a)
 
 a.concordance("impeachment")
 #print(Counter(a))
+"""
