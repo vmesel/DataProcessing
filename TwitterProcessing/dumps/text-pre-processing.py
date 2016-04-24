288adeecc10959b4feaf3a78214c783804b61145
Vinicius Mesel <vinicius@bigsp.com.br>
2016-04-24 00:07:18 -0300

    Links foram removidos

--- TwitterProcessing/dumps/text-pre-processing.py
+++ TwitterProcessing/dumps/text-pre-processing.py
@@ -7,6 +7,7 @@ regex = compile("\w+")
 arq = open("dbs/database.txt")
 
 # ----------- OP1
+"""
 artigos = "A O AS OS".split()
 
 for x in arq:
@@ -18,8 +19,9 @@ for x in arq:
             print(x[0], pessoa, resto,"\n")
     except:
         pass
-
+"""
 # ----------- OP2
+# Achar jeito de calcular quais palavras estão com maior frequência e que servem de adjetivos
 """
 lista = []
 for x in arq:
@@ -27,16 +29,25 @@ for x in arq:
     for y in saida:
         lista.append(y)
 
-print(Counter(lista))
+conta = Counter(lista)
+maisquemil = []
+for item in conta:
+    if conta[item] < 10:
+        pass
+    else:
+        maisquemil.append("[{},{}]".format(item,conta[item]))
+
+f = open("palavras-imp.txt","w")
+f.write(str(maisquemil))
+f.close()
 """
 # ----------- OP3
-"""
 texto = arq.read()
 
 a = nltk.wordpunct_tokenize(texto)
 
 a = nltk.Text(a)
 
-a.concordance("impeachment")
+a.concordance("economistas")
+
 #print(Counter(a))
-"""
